491757ee10483cdf542ee47f87043066a00f32c6
Aethor <oow.autre@gmail.com>
2025-04-23 15:04:19 +0800

    try to remove filtering for inference

--- models.py
+++ models.py
@@ -20,8 +20,7 @@ class TKBCModel(nn.Module, ABC):
         pass
 
     def get_ranking(
-            self, queries, filters, year2id = {},
-            batch_size: int = 1000, chunk_size: int = -1
+        self, queries, filters, year2id={}, batch_size: int = 1000, chunk_size: int = -1
     ):
         """
         Returns filtered ranking for each queries.
@@ -38,53 +37,92 @@ class TKBCModel(nn.Module, ABC):
             c_begin = 0
             while c_begin < self.sizes[2]:
                 b_begin = 0
-                rhs = self.get_rhs(c_begin, chunk_size)  # 将输入进来的训练集分为几个batch
+                rhs = self.get_rhs(
+                    c_begin, chunk_size
+                )  # 将输入进来的训练集分为几个batch
                 while b_begin < len(queries):
-                    if queries.shape[1] > 4:              # time intervals exist   对五元组中时间戳的处理
-                        these_queries = queries[b_begin:b_begin + batch_size]
+                    if (
+                        queries.shape[1] > 4
+                    ):  # time intervals exist   对五元组中时间戳的处理
+                        these_queries = queries[b_begin : b_begin + batch_size]
                         start_queries = []
                         end_queries = []
                         for triple in these_queries:
-                            if triple[3].split('-')[0] == '####':
+                            if triple[3].split("-")[0] == "####":
                                 start_idx = -1
                                 start = -5000
-                            elif triple[3][0] == '-':
-                                start = -int(triple[3].split('-')[1].replace('#', '0'))
-                            elif triple[3][0] != '-':
-                                start = int(triple[3].split('-')[0].replace('#','0'))
-                            if triple[4].split('-')[0] == '####':
+                            elif triple[3][0] == "-":
+                                start = -int(triple[3].split("-")[1].replace("#", "0"))
+                            elif triple[3][0] != "-":
+                                start = int(triple[3].split("-")[0].replace("#", "0"))
+                            if triple[4].split("-")[0] == "####":
                                 end_idx = -1
                                 end = 5000
-                            elif triple[4][0] == '-':
-                                end =-int(triple[4].split('-')[1].replace('#', '0'))
-                            elif triple[4][0] != '-':
-                                end = int(triple[4].split('-')[0].replace('#','0'))
-                            for key, time_idx in sorted(year2id.items(), key=lambda x:x[1]):        # 时间戳转换成id
-                                if start>=key[0] and start<=key[1]:
+                            elif triple[4][0] == "-":
+                                end = -int(triple[4].split("-")[1].replace("#", "0"))
+                            elif triple[4][0] != "-":
+                                end = int(triple[4].split("-")[0].replace("#", "0"))
+                            for key, time_idx in sorted(
+                                year2id.items(), key=lambda x: x[1]
+                            ):  # 时间戳转换成id
+                                if start >= key[0] and start <= key[1]:
                                     start_idx = time_idx
-                                if end>=key[0] and end<=key[1]:
+                                if end >= key[0] and end <= key[1]:
                                     end_idx = time_idx
 
-
                             if start_idx < 0:
-                                start_queries.append([int(triple[0]), int(triple[1])+self.sizes[1]//4, int(triple[2]), end_idx])
+                                start_queries.append(
+                                    [
+                                        int(triple[0]),
+                                        int(triple[1]) + self.sizes[1] // 4,
+                                        int(triple[2]),
+                                        end_idx,
+                                    ]
+                                )
                             else:
-                                start_queries.append([int(triple[0]), int(triple[1]), int(triple[2]), start_idx])
+                                start_queries.append(
+                                    [
+                                        int(triple[0]),
+                                        int(triple[1]),
+                                        int(triple[2]),
+                                        start_idx,
+                                    ]
+                                )
                             if end_idx < 0:
-                                end_queries.append([int(triple[0]), int(triple[1]), int(triple[2]), start_idx])
+                                end_queries.append(
+                                    [
+                                        int(triple[0]),
+                                        int(triple[1]),
+                                        int(triple[2]),
+                                        start_idx,
+                                    ]
+                                )
                             else:
-                                end_queries.append([int(triple[0]), int(triple[1])+self.sizes[1]//4, int(triple[2]), end_idx])
+                                end_queries.append(
+                                    [
+                                        int(triple[0]),
+                                        int(triple[1]) + self.sizes[1] // 4,
+                                        int(triple[2]),
+                                        end_idx,
+                                    ]
+                                )
 
-                        start_queries = torch.from_numpy(np.array(start_queries).astype('int64')).cuda()
-                        end_queries = torch.from_numpy(np.array(end_queries).astype('int64')).cuda()
+                        start_queries = torch.from_numpy(
+                            np.array(start_queries).astype("int64")
+                        ).cuda()
+                        end_queries = torch.from_numpy(
+                            np.array(end_queries).astype("int64")
+                        ).cuda()
 
                         q_s = self.get_queries(start_queries)
                         q_e = self.get_queries(end_queries)
                         scores = q_s @ rhs + q_e @ rhs
-                        targets = self.score(start_queries)+self.score(end_queries)
+                        targets = self.score(start_queries) + self.score(end_queries)
                     else:
-                        these_queries = queries[b_begin:b_begin + batch_size] # 500, 4
-                        q = self.get_queries(these_queries) # 500, 400
+                        these_queries = queries[
+                            b_begin : b_begin + batch_size
+                        ]  # 500, 4
+                        q = self.get_queries(these_queries)  # 500, 400
                         """
                         if use_left_queries:
                             lhs_queries = torch.ones(these_queries.size()).long().cuda()
@@ -97,8 +135,8 @@ class TKBCModel(nn.Module, ABC):
                             scores = q @ rhs +  q_lhs @ rhs
                             targets = self.score(these_queries) + self.score(lhs_queries)
                         """
-                        
-                        scores = q @ rhs 
+
+                        scores = q @ rhs
                         targets = self.score(these_queries)
 
                     assert not torch.any(torch.isinf(scores)), "inf scores"
@@ -109,21 +147,26 @@ class TKBCModel(nn.Module, ABC):
                     # set filtered and true scores to -1e6 to be ignored
                     # take care that scores are chunked
                     for i, query in enumerate(these_queries):
-                        if queries.shape[1]>4:
-                            filter_out = filters[int(query[0]), int(query[1]), query[3], query[4]]
-                            filter_out += [int(queries[b_begin + i, 2])]                            
-                        else:    
-                            filter_out = filters[(query[0].item(), query[1].item(), query[3].item())]
+                        if queries.shape[1] > 4:
+                            filter_out = filters.get(
+                                int(query[0]), int(query[1]), query[3], query[4], []
+                            )
+                            filter_out += [int(queries[b_begin + i, 2])]
+                        else:
+                            filter_out = filters.get(
+                                (query[0].item(), query[1].item(), query[3].item()), []
+                            )
                             filter_out += [queries[b_begin + i, 2].item()]
                         if chunk_size < self.sizes[2]:
                             filter_in_chunk = [
-                                int(x - c_begin) for x in filter_out
+                                int(x - c_begin)
+                                for x in filter_out
                                 if c_begin <= x < c_begin + chunk_size
                             ]
                             scores[i, torch.LongTensor(filter_in_chunk)] = -1e6
                         else:
                             scores[i, torch.LongTensor(filter_out)] = -1e6
-                    ranks[b_begin:b_begin + batch_size] += torch.sum(
+                    ranks[b_begin : b_begin + batch_size] += torch.sum(
                         (scores >= targets).float(), dim=1
                     ).cpu()
 
@@ -133,20 +176,27 @@ class TKBCModel(nn.Module, ABC):
         return ranks
 
 
-
 class TCompoundE(TKBCModel):
 
-    def __init__(self, sizes: Tuple[int, int, int, int], rank: int,no_time_emb=False, init_size: float = 1e-2):
+    def __init__(
+        self,
+        sizes: Tuple[int, int, int, int],
+        rank: int,
+        no_time_emb=False,
+        init_size: float = 1e-2,
+    ):
         super(TCompoundE, self).__init__()
         self.sizes = sizes
         self.rank = rank
-        self.W = nn.Embedding(2*rank, 1, sparse=True)
+        self.W = nn.Embedding(2 * rank, 1, sparse=True)
         self.W.weight.data *= 0
 
-        self.embeddings = nn.ModuleList([
-            nn.Embedding(s, 2 * rank, sparse=True)
-            for s in [sizes[0], sizes[1], sizes[3]] # without no_time_emb
-        ])
+        self.embeddings = nn.ModuleList(
+            [
+                nn.Embedding(s, 2 * rank, sparse=True)
+                for s in [sizes[0], sizes[1], sizes[3]]  # without no_time_emb
+            ]
+        )
         self.embeddings[0].weight.data *= init_size
         self.embeddings[1].weight.data *= init_size
         self.embeddings[2].weight.data *= init_size
@@ -157,61 +207,68 @@ class TCompoundE(TKBCModel):
     @staticmethod
     def has_time():
         return True
-	
+
     def score(self, x):
 
         lhs = self.embeddings[0](x[:, 0])
-        rel = self.embeddings[1](x[:, 1]) 
+        rel = self.embeddings[1](x[:, 1])
         rhs = self.embeddings[0](x[:, 2])
         time = self.embeddings[2](x[:, 3])
 
-        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
-        rel = rel[:, :self.rank] / ( 1 / self.pi), rel[:, self.rank:] / ( 1 / self.pi)
-        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
-        time = time[:, :self.rank], time[:, self.rank:]
+        lhs = lhs[:, : self.rank], lhs[:, self.rank :]
+        rel = rel[:, : self.rank] / (1 / self.pi), rel[:, self.rank :] / (1 / self.pi)
+        rhs = rhs[:, : self.rank], rhs[:, self.rank :]
+        time = time[:, : self.rank], time[:, self.rank :]
 
-        rt = (rel[0] + time[0]) * time[1], rel[1]        
-        return torch.sum(
-            ( (lhs[0] + rt[1]) * rt[0] ) * rhs[0], 1, keepdim=True)
-	
+        rt = (rel[0] + time[0]) * time[1], rel[1]
+        return torch.sum(((lhs[0] + rt[1]) * rt[0]) * rhs[0], 1, keepdim=True)
+
     def forward(self, x):
 
         lhs = self.embeddings[0](x[:, 0])
-        rel = self.embeddings[1](x[:, 1]) 
+        rel = self.embeddings[1](x[:, 1])
         rhs = self.embeddings[0](x[:, 2])
         time = self.embeddings[2](x[:, 3])
 
-        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
-        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
-        rel = rel[:, :self.rank] / ( 1 / self.pi), rel[:, self.rank:] / ( 1 / self.pi)
-        time = time[:, :self.rank], time[:, self.rank:]
+        lhs = lhs[:, : self.rank], lhs[:, self.rank :]
+        rhs = rhs[:, : self.rank], rhs[:, self.rank :]
+        rel = rel[:, : self.rank] / (1 / self.pi), rel[:, self.rank :] / (1 / self.pi)
+        time = time[:, : self.rank], time[:, self.rank :]
 
         right = self.embeddings[0].weight
-        right = right[:, :self.rank], right[:, self.rank:]
+        right = right[:, : self.rank], right[:, self.rank :]
 
         rt = (rel[0] + time[0]) * time[1], rel[1]
 
         return (
-                    ((lhs[0] + rt[1]) * rt[0] ) @ right[0].t()
-               ), (
-                   torch.sqrt(lhs[0] ** 2),
-                   torch.sqrt(rt[0] ** 2 + rt[1] ** 2),
-                   torch.sqrt(rhs[0] ** 2)
-               ),  self.embeddings[2].weight[:-1] if self.no_time_emb else self.embeddings[2].weight
-
+            (((lhs[0] + rt[1]) * rt[0]) @ right[0].t()),
+            (
+                torch.sqrt(lhs[0] ** 2),
+                torch.sqrt(rt[0] ** 2 + rt[1] ** 2),
+                torch.sqrt(rhs[0] ** 2),
+            ),
+            (
+                self.embeddings[2].weight[:-1]
+                if self.no_time_emb
+                else self.embeddings[2].weight
+            ),
+        )
 
     def get_rhs(self, chunk_begin: int, chunk_size: int):
-        return self.embeddings[0].weight.data[chunk_begin:chunk_begin + chunk_size][:, :self.rank].transpose(0, 1)
+        return (
+            self.embeddings[0]
+            .weight.data[chunk_begin : chunk_begin + chunk_size][:, : self.rank]
+            .transpose(0, 1)
+        )
 
     def get_queries(self, queries: torch.Tensor):
         lhs = self.embeddings[0](queries[:, 0])
-        rel = self.embeddings[1](queries[:, 1]) 
+        rel = self.embeddings[1](queries[:, 1])
         time = self.embeddings[2](queries[:, 3])
-        
-        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
-        rel = rel[:, :self.rank] / ( 1 / self.pi), rel[:, self.rank:] / ( 1 / self.pi)
-        time = time[:, :self.rank], time[:, self.rank:]
+
+        lhs = lhs[:, : self.rank], lhs[:, self.rank :]
+        rel = rel[:, : self.rank] / (1 / self.pi), rel[:, self.rank :] / (1 / self.pi)
+        time = time[:, : self.rank], time[:, self.rank :]
 
         rt = (rel[0] + time[0]) * time[1], rel[1]
         return (lhs[0] + rt[1]) * rt[0]
-
